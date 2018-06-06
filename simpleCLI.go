23f2912669f5ae32c29433d5621b675749d8c20f
Araan Branco <araan666@gmail.com>
2018-06-06 16:08:59 -0300

    Up project :ok_hand:

---
+++ simpleCLI.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "simpleCLI"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "provider, p",
+			Usage: "Call the provider 'aws'",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "aws",
+			Usage: "commands for AWS",
+			Action: func(c *cli.Context) error {
+				services.ec2Service(c.Args())
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
